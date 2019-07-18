655fb474108291d8c5af8e10f8e654fbeb0aa78d
Qin <liuqin0821@gmail.com>
2019-07-18 18:12:46 +0800

    首页商品列表init
    描述: 无
    性能影响: 无
    测试场景: 本地

--- app/src/main/java/com/simpure/expires/ui/CommodityHomeActivity.kt
+++ app/src/main/java/com/simpure/expires/ui/CommodityHomeActivity.kt
@@ -1,12 +1,17 @@
 package com.simpure.expires.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import com.simpure.expires.R
 import com.simpure.expires.data.*
 import com.simpure.expires.databinding.ActivityHomeBinding
 import kotlinx.android.synthetic.main.activity_home.*
+
 
 class CommodityHomeActivity : BaseActivity() {
 
@@ -27,8 +32,51 @@ class CommodityHomeActivity : BaseActivity() {
 
     private lateinit var commodityList: List<CommodityRepository>
     private lateinit var commodity: CommodityHomeRepository
+    private lateinit var fragments: List<Fragment>
 
     private lateinit var binding: ActivityHomeBinding
+
+    private var lastPosition = -1
+
+
+    private val internalPageChangeListener = object : OnPageChangeListener {
+        override fun onPageScrollStateChanged(state: Int) {
+            Log.d(javaClass.simpleName, "state: $state")
+        }
+
+        override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+        }
+
+        override fun onPageSelected(position: Int) {
+            /*if (vpCommodityList.adapter == null || vpCommodityList.adapter.count <= 0) return
+
+            if (mAnimatorIn.isRunning()) {
+                mAnimatorIn.end()
+                mAnimatorIn.cancel()
+            }
+
+            if (mAnimatorOut.isRunning()) {
+                mAnimatorOut.end()
+                mAnimatorOut.cancel()
+            }
+
+            val currentIndicator: View
+            if (mLastPosition >= 0 && (currentIndicator = getChildAt(mLastPosition)) != null) {//页面离开屏幕时指示器动画
+                currentIndicator.setBackgroundResource(mIndicatorUnselectedBackgroundResId)
+                mAnimatorIn.setTarget(currentIndicator)
+                mAnimatorIn.start()
+            }
+
+            val selectedIndicator = getChildAt(position)
+            if (selectedIndicator != null) {//页面进入屏幕时指示器动画
+                selectedIndicator!!.setBackgroundResource(mIndicatorBackgroundResId)
+                mAnimatorOut.setTarget(selectedIndicator)
+                mAnimatorOut.start()
+            }
+            lastPosition = position*/
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,6 +97,15 @@ class CommodityHomeActivity : BaseActivity() {
         )
         rlvBoxes.adapter = PlaceNameAdapter(placeList)
         rlvBoxes.setOffscreenItems(3)
+
+        if (vpCommodityList.adapter != null) {
+            vpCommodityList.adapter = CommodityFragmentAdapter(fragments)
+            lastPosition = -1
+//            createIndicators()
+            vpCommodityList.removeOnPageChangeListener(internalPageChangeListener)
+            vpCommodityList.addOnPageChangeListener(internalPageChangeListener)
+            internalPageChangeListener.onPageSelected(vpCommodityList.currentItem)
+        }
 //            setContentView(R.layout.activity_home)
 //        mainViewModel = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
 //        binding.mainViewModel = mainViewModel
@@ -61,6 +118,7 @@ class CommodityHomeActivity : BaseActivity() {
             listOf(BoxRepository("box", commodityList))
         )
     }
+
 
     private fun getExpiredList(): List<CommodityRepository> {
 
