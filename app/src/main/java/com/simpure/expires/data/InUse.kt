c04801439f209e0ae31ff95c03fe0bd0730e16b5
Qin <liuqin0821@gmail.com>
2019-09-23 17:55:36 +0800

    整理Commodity表 part1
    描述：
    思路：
    测试：
    Fixes: NMK-

--- app/src/main/java/com/simpure/expires/data/InUse.kt
+++ app/src/main/java/com/simpure/expires/data/InUse.kt
@@ -1,7 +1,7 @@
 package com.simpure.expires.data
 
 data class InUse(
-    val num: Int,
-    val date: Long,
-    val unboxingDate: Long
+    val num: Int = 0,
+    val date: Long = 0L,
+    val unboxingDate: Long = 0L
 )
--- app/src/main/java/com/simpure/expires/data/entry/CommodityEntity.kt
+++ app/src/main/java/com/simpure/expires/data/entry/CommodityEntity.kt
@@ -4,19 +4,41 @@ import androidx.room.Entity
 import androidx.room.Ignore
 import androidx.room.PrimaryKey
 import com.simpure.expires.data.CommodityType
+import com.simpure.expires.data.InUse
 
 import com.simpure.expires.model.Commodity
+import com.simpure.expires.model.CommodityDetail
+import com.simpure.expires.model.Inventory
 import com.simpure.expires.utilities.calcExpirationDate
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
 
 @Entity(tableName = "commodities")
-class CommodityEntity : Commodity {
+class CommodityEntity @Ignore constructor(
     @PrimaryKey
-    override var id: Int = 0
-    override var name: String = ""
-    override var expirationDate: Long = 0L
-//    override var type: String = calcType()
+    override var id: Int, override var name: String, override var expirationDate: Long,
+    
+) : CommodityDetail {
+    override val unit: String
+        get() = ""
+    override val place: String
+        get() = ""
+    override val labels: List<String>
+        get() = listOf()
+    override val barcode: String
+        get() = ""
+    override val inventories: List<Inventory>
+        get() = listOf()
+    override val amount: Int
+        get() = 0
+    override val inUse: InUse
+        get() = InUse()
+    override val unboxingDuration: Long
+        get() = 0L
+    override val usedList: List<CommodityDetail>
+        get() = listOf()
+    override val disable: Boolean
+        get() = false
 
     override fun calc(): String {
         return expirationDate.calcExpirationDate().toString()
@@ -36,20 +58,5 @@ class CommodityEntity : Commodity {
         return date.toString(DateTimeFormat.mediumDate())
     }
 
-
-    constructor() {}
-
-    @Ignore
-    constructor(id: Int, name: String, expirationDate: Long) {
-        this.id = id
-        this.name = name
-        this.expirationDate = expirationDate
-    }
-
-    constructor(commodity: Commodity) {
-        this.id = commodity.id
-        this.name = commodity.name
-        this.expirationDate = commodity.expirationDate
-    }
 }
 
